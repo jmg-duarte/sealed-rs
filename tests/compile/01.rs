10418be7b5ab76924ebc722d163a7a5797b34d88
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-17 14:14:42 +0000

    Clean up test

--- tests/compile/01.rs
+++ tests/compile/01.rs
@@ -2,19 +2,13 @@ use sealed::sealed;
 
 #[sealed]
 pub struct A;
-// impl private::Sealed for crate::A {}
 #[sealed]
 pub struct B{
     field_1: i32
 }
-// impl private::Sealed for crate::B {}
 
 #[sealed]
-trait T {} // : private::Sealed {}
-// generates
-// mod private {
-//     pub trait Sealed {}
-// }
+trait T {}
 
 impl T for A {}
 impl T for B {}
