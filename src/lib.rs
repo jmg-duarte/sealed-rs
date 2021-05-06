fb569d3246a679f5428fae3825865d1912b06c7f
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-05-06 16:38:40 +0100

    Rework the way sealed items are handled

--- src/lib.rs
+++ src/lib.rs
@@ -63,8 +63,34 @@
 //! ```
 
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, parse_quote};
+
+macro_rules! SEAL_FORMAT {
+    () => {
+        "__seal_for_{}"
+    };
+}
+
+macro_rules! parse_quote {(
+    $($code:tt)*
+) => (
+    (|| {
+        fn type_of_some<T> (_: Option<T>)
+          -> &'static str
+        {
+            ::core::any::type_name::<T>()
+        }
+        let target_ty = None; if false { return target_ty.unwrap(); }
+        eprintln!(
+            "[{}:{}:{}:parse_quote!]\n  - ty: `{ty}`\n  - code: `{code}`",
+            file!(), line!(), column!(),
+            code = ::quote::quote!( $($code)* ),
+            ty = type_of_some(target_ty),
+        );
+        ::syn::parse_quote!( $($code)* )
+    })()
+)}
 
 #[proc_macro_attribute]
 pub fn sealed(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -77,7 +103,8 @@ pub fn sealed(_args: TokenStream, input: TokenStream) -> TokenStream {
 
 fn parse_sealed(item: syn::Item) -> syn::Result<proc_macro2::TokenStream> {
     match item {
-        syn::Item::Struct(item_struct) => parse_sealed_struct(item_struct),
+        syn::Item::Impl(item_impl) => parse_sealed_impl(item_impl),
+        // syn::Item::Struct(item_struct) => parse_sealed_struct(item_struct),
         syn::Item::Trait(item_trait) => parse_sealed_trait(item_trait),
         _ => Err(syn::Error::new(
             proc_macro2::Span::call_site(),
@@ -86,22 +113,49 @@ fn parse_sealed(item: syn::Item) -> syn::Result<proc_macro2::TokenStream> {
     }
 }
 
+// // Care for https://gist.github.com/Koxiaet/8c05ebd4e0e9347eb05f265dfb7252e1#procedural-macros-support-renaming-the-crate
+// fn parse_sealed_struct(strct: syn::ItemStruct) -> syn::Result<proc_macro2::TokenStream> {
+//     let ident = &strct.ident;
+//     Ok(quote!(
+//         #strct
+//         impl private::Sealed for #ident {}
+//     ))
+// }
+
 // Care for https://gist.github.com/Koxiaet/8c05ebd4e0e9347eb05f265dfb7252e1#procedural-macros-support-renaming-the-crate
-fn parse_sealed_struct(strct: syn::ItemStruct) -> syn::Result<proc_macro2::TokenStream> {
-    let ident = &strct.ident;
+fn parse_sealed_trait(mut item_trait: syn::ItemTrait) -> syn::Result<proc_macro2::TokenStream> {
+    let trait_ident = &item_trait.ident;
+    let seal = format_ident!(SEAL_FORMAT!(), trait_ident);
+    item_trait.supertraits.push(parse_quote!(#seal::Sealed));
     Ok(quote!(
-        #strct
-        impl private::Sealed for #ident {}
+        pub(crate) mod #seal {
+            pub trait Sealed {}
+        }
+        #item_trait
     ))
 }
 
-// Care for https://gist.github.com/Koxiaet/8c05ebd4e0e9347eb05f265dfb7252e1#procedural-macros-support-renaming-the-crate
-fn parse_sealed_trait(mut trt: syn::ItemTrait) -> syn::Result<proc_macro2::TokenStream> {
-    trt.supertraits.push(parse_quote!(private::Sealed));
-    Ok(quote!(
-        #trt
-        mod private {
-            pub trait Sealed {}
-        }
-    ))
+fn parse_sealed_impl(item_impl: syn::ItemImpl) -> syn::Result<proc_macro2::TokenStream> {
+    eprintln!("{:#?}", item_impl);
+    if let Some(impl_trait) = &item_impl.trait_ {
+        let mut sealed_path = impl_trait.1.segments.clone();
+        // since `impl for ...` is not allowed, this path will *always* have at least length 1
+        // thus both `first` and `last` are safe to unwrap
+        let trait_ident = sealed_path.pop().unwrap().into_value().ident;
+
+        let seal = format_ident!(SEAL_FORMAT!(), trait_ident);
+        sealed_path.push(parse_quote!(#seal));
+        sealed_path.push(parse_quote!(Sealed));
+
+        let self_type = &item_impl.self_ty;
+        Ok(quote! {
+            impl #sealed_path for #self_type {}
+            #item_impl
+        })
+    } else {
+        Err(syn::Error::new_spanned(
+            item_impl,
+            "missing implentation trait",
+        ))
+    }
 }
