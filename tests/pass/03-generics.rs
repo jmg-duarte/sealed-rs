55ee9e8263cec1c9174ee074a5ba11252c9b5fa6
José Duarte <jmg.duarte@campus.fct.unl.pt>
2021-05-06 17:35:50 +0100

    Add tests and examples for generics

---
+++ tests/pass/03-generics.rs
@@ -0,0 +1,22 @@
+// Test provided in #4
+// https://github.com/jmg-duarte/sealed-rs/issues/4
+
+use sealed::sealed;
+
+#[sealed]
+pub trait Set<V> {}
+// pub trait Set<V>: private::Sealed<V> {}
+// mod private {
+//     pub trait Sealed<V> {}
+// }
+
+#[sealed]
+impl<T> Set<Option<T>> for T {}
+// impl<T> private::Sealed<Option<T>> for T {}
+
+#[sealed]
+impl<T> Set<Option<T>> for Option<T> {}
+// impl<T> private::Sealed<Option<T>> for Option<T> {}
+
+
+fn main() {}
