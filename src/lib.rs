4e1106d79e8006ed3e11bf5377034c958d87c7c6
José Duarte <jmg.duarte@campus.fct.unl.pt>
2022-01-30 20:19:49 +0000

    detail review

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@
 //!
 //! [<img alt="" src="https://img.shields.io/badge/docs.rs-sealed-success?style=flat-square">](https://docs.rs/sealed)
 //! [<img alt="" src="https://img.shields.io/crates/v/sealed?style=flat-square">](https://crates.io/crates/sealed)
+//! <img alt="MSRV 1.51.0" src="https://img.shields.io/badge/msrv-1.51.0-blue?style=flat-square">
 //!
 //! This crate provides a convenient and simple way to implement the sealed trait pattern,
 //! as described in the Rust API Guidelines [[1](https://rust-lang.github.io/api-guidelines/future-proofing.html#sealed-traits-protect-against-downstream-implementations-c-sealed)].
 //!
 //! ```toml
 //! [dependencies]
-//! sealed = "0.3"
+//! sealed = "0.4"
 //! ```
 //!
 //! ## Example
@@ -42,7 +43,7 @@
 //! trait definition, when attached to an `impl` block the generated code simply
 //! implements the sealed trait for the respective type.
 //!
-//! ```rust
+//! ```rust,ignore
 //! // #[sealed]
 //! // trait T {}
 //! trait T: __seal_t::Sealed {}
@@ -68,7 +69,7 @@
 //! attribute inside a function. By default, all the bounds are propagated to
 //! the generated `Sealed` trait.
 //!
-//! ```rust
+//! ```rust,ignore
 //! // #[sealed(erase)]
 //! // trait Trait<T: ?Sized + Default> {}
 //! trait Trait<T: ?Sized + Default>: __seal_trait::Sealed<T> {}
